fac71d70f938dc27729d74c2b17cddf1ec44d7c4
AkshayParsade <adparsade@gmail.com>
2025-02-15 18:13:38 +0530

    --- Auto Git Commit ---

--- EC2/autoscalling.tf
+++ EC2/autoscalling.tf
@@ -2,7 +2,7 @@ resource "aws_launch_configuration" "lc-home" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_name
-    security_groups =
-    user_data = 
+    security_groups = var.security_groups
+    user_data = var.user_data
   
 }
