6af315c4b3231149df39847b4719bfee4df09b9f
AkshayParsade <adparsade@gmail.com>
2025-02-15 15:39:07 +0530

    ec2 update

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+
+resource "aws_instance" "my_ec2" {
+    ami = "ami-00bb6a80f01f03502"
+    instance_type = "t2.micro"
+    key_name = "Ubuntu"
+    security_groups = "default"
+    tags = {
+      "name" = "tf_instance"
+    }
+  
+}
