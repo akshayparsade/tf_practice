198a81c4a8a8fb4050b21e70a65ef5c36dc6b0dd
AkshayParsade <adparsade@gmail.com>
2025-02-15 18:21:39 +0530

    --- Auto Git Commit ---

--- EC2/autoscalling.tf
+++ EC2/autoscalling.tf
@@ -12,20 +12,3 @@ resource "aws_launch_configuration" "lc-home" {
     EOF    
 }
 
-resource "aws_security_group" "lc_home" {
-  name = "allow httpd "
-    description = "HTTP port"
-    ingress {                               
-        from_port        = 80
-        to_port          = 80
-        protocol         = "-1"  
-        cidr_blocks      = ["0.0.0.0/0"]
-    }
-    egress {                             
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }                                   
-  
-}
