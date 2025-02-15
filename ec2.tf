80f62a728eb14d51111809866b65a36e9fb56732
AkshayParsade <adparsade@gmail.com>
2025-02-15 15:47:22 +0530

    ec2 update

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "my_ec2" {
     ami = "ami-00bb6a80f01f03502"
     instance_type = "t2.micro"
     key_name = "Ubuntu"
-    security_groups = "default"
+    security_groups = ["default"]
     tags = {
       "name" = "tf_instance"
     }
