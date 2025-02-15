f68c006b83f403e1ab9d97c2c8391d3f91caf2c8
AkshayParsade <adparsade@gmail.com>
2025-02-15 19:05:33 +0530

    --- Auto Git Commit ---

--- EC2/loadbalancer.tf
+++ EC2/loadbalancer.tf
@@ -43,7 +43,7 @@ resource "aws_lb" "my_lb" {
   }
 }
 
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "" {
   load_balancer_arn = aws_lb.front_end.arn
   port              = "443"
   protocol          = "HTTPS"
