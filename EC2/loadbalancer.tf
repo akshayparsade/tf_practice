90d8cdfc82993d0d1b096884118b9e87e7c108ed
AkshayParsade <adparsade@gmail.com>
2025-02-15 19:09:42 +0530

    --- Auto Git Commit ---

--- EC2/loadbalancer.tf
+++ EC2/loadbalancer.tf
@@ -54,7 +54,7 @@ resource "aws_lb_listener" "my_lb_lisener" {
   }
 }
 
-resource "aws_lb_listener_rule" "my_" {
+resource "aws_lb_listener_rule" "my_lb_lisener_rule" {
   listener_arn = aws_lb_listener.front_end.arn
   priority     = 100
 
