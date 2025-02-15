5f47c5452a09ab3fb4f29ad0d11b8ba857b87353
AkshayParsade <adparsade@gmail.com>
2025-02-15 16:12:07 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -3,13 +3,40 @@ provider "aws" {
   
 }
 
+# resource "aws_instance" "my_ec2" {
+#     ami = "ami-00bb6a80f01f03502"
+#     instance_type = "t2.micro"
+#     key_name = "Ubuntu"
+#     security_groups = ["default"]
+#     tags = {
+#       "name" = "tf_instance"
+#     }
+  
+# }
+
 resource "aws_instance" "my_ec2" {
-    ami = "ami-00bb6a80f01f03502"
-    instance_type = "t2.micro"
-    key_name = "Ubuntu"
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
     security_groups = ["default"]
     tags = {
       "name" = "tf_instance"
     }
+  
+}
+
+variable "ami" {
+    type = string
+    default = "ami-00bb6a80f01f03502"
+    description = "Enter ami id for ap-south-1"
+}
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+    description = "instance_type:t2.micro"
+
+}
+variable "key_name" {
+    default = "Ubuntu"
   
 }
