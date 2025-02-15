eec434c849a2f5e6bc644f943ff3427891867fe7
AkshayParsade <adparsade@gmail.com>
2025-02-15 19:02:52 +0530

    --- Auto Git Commit ---

--- EC2/loadbalancer.tf
+++ EC2/loadbalancer.tf
@@ -36,7 +36,7 @@ resource "aws_lb" "my_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.my_sg.id]
-  subnets            = [var.subnet_id_1, va]
+  subnets            = [var.subnet_id_1, var.subnet_id_2]
 
   enable_deletion_protection = true
 
