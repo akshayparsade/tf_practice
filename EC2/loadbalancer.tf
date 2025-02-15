de7658d54e169a3ccbf96e900c88cf367fd01a15
AkshayParsade <adparsade@gmail.com>
2025-02-15 19:57:38 +0530

    --- Auto Git Commit ---

--- EC2/loadbalancer.tf
+++ EC2/loadbalancer.tf
@@ -2,7 +2,7 @@ resource "aws_lb_target_group" "tg_home" {
   name     = "tg_home"
   port     = 80
   protocol = "HTTP"
-  vpc_id = 
+  vpc_id = var.vpc_id
   health_check {
     path = "/"
     port = 80
