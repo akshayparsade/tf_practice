dd0ebd61ef083565091204677ab7eeb8380fe858
AkshayParsade <adparsade@gmail.com>
2025-02-15 18:25:24 +0530

    --- Auto Git Commit ---

--- EC2/autoscalling.tf
+++ EC2/autoscalling.tf
@@ -41,7 +41,7 @@ resource "aws_launch_configuration" "lc-laptop" {
         apt install httpd -y
         systemctl start httpd
         systemctl enable httpd
-        mkdir 
+        mkdir /var/
         echo <h1> Welcome to Akkitech </h1> > /var/wwww/html/index.html
     EOF    
 }
