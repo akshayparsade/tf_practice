a2a28bbdf851400f75cef6c0f771170e25745a5f
AkshayParsade <adparsade@gmail.com>
2025-02-15 16:59:58 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "my_ec2" {
            yum install httpd -y
            systemctl start httpd
            systemctl enable httpd
-           EOF   
+    EOF   
 }
 
 variable "ami" {
