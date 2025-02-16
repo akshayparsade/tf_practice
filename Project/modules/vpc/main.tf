2852d7c46604b819a7459840ca49574084f85203
AkshayParsade <adparsade@gmail.com>
2025-02-16 08:53:27 +0530

    --- Auto Git Commit ---

--- Project/modules/vpc/main.tf
+++ Project/modules/vpc/main.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "private-subnet" {
 
 resource "aws_subnet" "public-subnet" {
   vpc_id     = aws_vpc.my-vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = var.pub
 
   tags = {
     Name = "${var.project}-public-subnet"
