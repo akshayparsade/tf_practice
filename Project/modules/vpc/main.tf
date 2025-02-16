67965df1b4785b90eb5100b3a6a9abf6dcd47ae8
AkshayParsade <adparsade@gmail.com>
2025-02-16 08:45:25 +0530

    --- Auto Git Commit ---

--- Project/modules/vpc/main.tf
+++ Project/modules/vpc/main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "sub-1a" {
 
 resource "aws_subnet" "sub-1b" {
   vpc_id     = aws_vpc.my-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
 
   tags = {
     Name = "Main"
