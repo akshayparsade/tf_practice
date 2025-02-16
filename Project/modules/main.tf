6d5d88d26be49302d100687b1d29ef94984e4846
AkshayParsade <adparsade@gmail.com>
2025-02-16 09:17:00 +0530

    --- Auto Git Commit ---

--- Project/modules/main.tf
+++ Project/modules/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 module "vpc_module" {
     source = "./modules/vpc"
-    cidr =
+    cidr = var.vpc_cidr
     project = var.project
     env = var.env
     pri-sub-cidr = var.pri_sub_cidr
