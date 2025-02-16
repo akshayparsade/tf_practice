b43ad37b718756bdebc9c8e0d8ae638e9cecf3e4
AkshayParsade <adparsade@gmail.com>
2025-02-16 08:40:44 +0530

    --- Auto Git Commit ---

--- Project/modules/vpc/main.tf
+++ Project/modules/vpc/main.tf
@@ -14,3 +14,21 @@ resource "aws_internet_gateway" "gw" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
