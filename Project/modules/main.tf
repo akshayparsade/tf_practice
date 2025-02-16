74234a4e7ee6333e7b7fd345fa36b4ddd8f5abaa
AkshayParsade <adparsade@gmail.com>
2025-02-16 09:12:43 +0530

    --- Auto Git Commit ---

--- Project/modules/main.tf
+++ Project/modules/main.tf
@@ -7,6 +7,7 @@ module "vpc_module" {
     source = "./modules/vpc"
     cidr ="10.0.0.0/16"
     project = "cbz"
+    env = "env"
     pri-sub-cidr = "10.0.1.0/16"
     pub-sub-cidr = "10.0.2.0/16"
 }
