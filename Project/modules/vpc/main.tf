496f9f59c7c0ee42dac7dc36c87c789a4103f2dd
AkshayParsade <adparsade@gmail.com>
2025-02-16 08:43:01 +0530

    --- Auto Git Commit ---

--- Project/modules/vpc/main.tf
+++ Project/modules/vpc/main.tf
@@ -53,3 +53,12 @@ resource "aws_route_table" "example" {
   }
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
