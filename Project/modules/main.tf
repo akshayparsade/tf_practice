526418a8ed8335c415e8ef505c71532c84a77963
AkshayParsade <adparsade@gmail.com>
2025-02-16 09:28:14 +0530

    --- Auto Git Commit ---

--- Project/modules/main.tf
+++ Project/modules/main.tf
@@ -15,5 +15,9 @@ module "vpc_module" {
 module "ec2_module" {
     source = "./modules/ec2"
     ami = var.image_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.security_group_id
+    key_name = var.key_pair
+    subnet_id = var.private_subnet_id
   
 }
