3d82af13f39b183067a278f7bdcea732386a5748
AkshayParsade <adparsade@gmail.com>
2025-02-16 09:23:49 +0530

    --- Auto Git Commit ---

--- Project/modules/ec2/main.tf
+++ Project/modules/ec2/main.tf
@@ -6,3 +6,10 @@ resource "aws_instance" "private_instance" {
     subnet_id = var.private_subnet_id
 }
 
+resource "aws_instance" "public_instance" {
+    ami = var.image_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.security_group_id
+    key_name = var.key_pair
+    subnet_id = var.private_subnet_id
+}
