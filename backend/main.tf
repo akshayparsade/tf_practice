68b51b8a25bdb543abdf37a5be84fe112996187b
AkshayParsade <adparsade@gmail.com>
2025-06-20 17:00:35 +0530

    name chmage

--- backend/main.tf
+++ backend/main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "s3_bucket" {
  }
 }
 
-resource "aws_dynamodb_table" "basic-aws_dynamodb_table" {
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
  name = "tf-eks-state-locks" 
  billing_mode = "PAY_PER_REQUEST"
  read_capacity = 20
