39a1179e0d1894ed0405f6ccd79070d6a2a454f4
AkshayParsade <adparsade@gmail.com>
2025-06-25 12:58:50 +0530

    new changes

--- backend/main.tf
+++ backend/main.tf
@@ -1,27 +1,39 @@
 provider "aws" {
-    region = "ap-south-1"
-  
+  region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
- bucket = "demo-tf-eks-state-bucket-akki19"
- force_destroy = true
- lifecycle {
-   prevent_destroy = false
- }
-}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "demo-terraform-eks-state-s3-bux"
 
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
- name = "tf-eks-state-locks" 
- billing_mode = "PAY_PER_REQUEST"
- hash_key = "LockID"
-
- attribute {
-   name = "LockID"
-   type = "S"
- }
-tags = {
-    Name        = "Terraform Lock Table"
-    Environment = "Dev"
+  lifecycle {
+    prevent_destroy = false
   }
-} 
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-eks-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
