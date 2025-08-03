c8bb063e4b51065e8934b2a62820a893421802ca
AkshayParsade <adparsade@gmail.com>
2025-08-03 14:46:05 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2.tf
+++ NEW_PRACTICE/ec2.tf
@@ -10,7 +10,8 @@ resource "aws_security_group" "ec2_sg" {
     ingress {
         from_port = 80
         to_port = 0
-        protocol = 
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
     from_port        = 0
