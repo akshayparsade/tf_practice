1a932641960602ee2975d8288f3542a5ed4795cc
AkshayParsade <adparsade@gmail.com>
2025-08-03 14:31:22 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2.tf
+++ NEW_PRACTICE/ec2.tf
@@ -12,5 +12,8 @@ resource "aws_instance" "instance_1" {  # name must start with a letter or under
     instance_type = "t2.micro"
     key_name = "New_Virgi_key"
     security_groups = ["default"]
-
 }  
+
+variable "ami" {
+  
+}
