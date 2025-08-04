7ab58792624b68cfdbfa4354e925c400fa568f4f
AkshayParsade <adparsade@gmail.com>
2025-08-04 10:49:22 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/autoscalling.tf
+++ NEW_PRACTICE/ec2/autoscalling.tf
@@ -4,11 +4,10 @@ resource "aws_security_group" "lc_sg" {
    vpc_id      = aws_vpc.main.id
 
    ingress {
-    from_port        = 0
+    from_port        = 80
     to_port          = 0
-    protocol         = "-1"
+    protocol         = "TCP"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
