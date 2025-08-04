a74715666a30d34fb829edfede0287b8346db090
AkshayParsade <adparsade@gmail.com>
2025-08-04 14:03:14 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/Lbr.tf
+++ NEW_PRACTICE/ec2/Lbr.tf
@@ -52,6 +52,11 @@ resource "aws_lb_listener" "my_lb_listener_rule_groccary" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg_groccary
   }
+  condition {
+    path_pattern {
+      values = ["/elctronix/*"]
+    }
+  }
 }
 
 resource "aws_lb_listener_rule" "my_lb_listener_rule_elctronix" {
