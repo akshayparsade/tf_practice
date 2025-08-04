9b01294ba79e3dde189ee6e7a5096e9a38eed628
AkshayParsade <adparsade@gmail.com>
2025-08-04 16:24:17 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/groccary.sh
+++ NEW_PRACTICE/ec2/groccary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
 apt install httpd -y
 systemctl start httpd
 systemctl enable httpd
 mkdir /var/www/html/groccary
-echo "<h1> This is groccary Page  </h1>" > /var/www/html/index.html
-    EOF 
+echo "<h1> This is groccary Page  </h1>" > /var/www/html/index.html 
