5b753e9af8eac6ac86c50498b8725ee39f88fffd
AkshayParsade <adparsade@gmail.com>
2025-08-04 16:26:17 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/autoscalling.tf
+++ NEW_PRACTICE/ec2/autoscalling.tf
@@ -19,7 +19,7 @@ resource "aws_launch_template" "lc_home" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name =var.key_name
-    vpc_security_groups_ids = [ aws_security_group.lc_sg.id ]
+    vpc_security_groups_ids = [ aws_security_group.lc_sg.id]
     user_data = filebase64("./home.sh")
 }
 
