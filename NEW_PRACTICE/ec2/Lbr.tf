bea743594b0e10d2e22be2877e0e659f012c07f4
AkshayParsade <adparsade@gmail.com>
2025-08-04 13:54:21 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/Lbr.tf
+++ NEW_PRACTICE/ec2/Lbr.tf
@@ -48,8 +48,6 @@ resource "aws_lb_listener" "my_lb_listener" {
   load_balancer_arn = aws_lb.my_lb.arn
   port              = "80"
   protocol          = "HTTP"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
-
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.front_end.arn
