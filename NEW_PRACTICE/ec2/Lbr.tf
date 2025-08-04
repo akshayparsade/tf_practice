d05f2ec7e364d80bd27516881c0da81902e8fce9
AkshayParsade <adparsade@gmail.com>
2025-08-04 14:08:45 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/Lbr.tf
+++ NEW_PRACTICE/ec2/Lbr.tf
@@ -70,7 +70,7 @@ resource "aws_lb_listener_rule" "my_lb_listener_rule_elctronix" {
 
 resource "aws_lb_listener_rule" "my_lb_listener_rule_groccary" {
   listener_arn = aws_lb_listener.my_lb_listener.arn
-  priority     = 100
+  priority     = 101
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg_groccary.arn
