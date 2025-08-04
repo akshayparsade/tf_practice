b5912adad051af74d792b16f3ab69102e621ca0a
AkshayParsade <adparsade@gmail.com>
2025-08-04 16:22:06 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/home.sh
+++ NEW_PRACTICE/ec2/home.sh
@@ -2,6 +2,6 @@
 
 apt install httpd -y
 systemctl start httpd
-    systemctl enable httpd
-    echo "<h1> Hello World </h1> <br> <h2>Welcome to akkitech </h2>" > /var/www/html/index.html
+systemctl enable httpd
+echo "<h1> Hello World </h1> <br> <h2>Welcome to akkitech </h2>" > /var/www/html/index.html
     
