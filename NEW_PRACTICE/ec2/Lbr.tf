7804f1ac2879e7670dc058ee1970e0ded3654d8a
AkshayParsade <adparsade@gmail.com>
2025-08-04 13:41:38 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/Lbr.tf
+++ NEW_PRACTICE/ec2/Lbr.tf
@@ -8,3 +8,25 @@ resource "aws_lb_target_group" "tg_home" {
     protocol = "TCP"
   }
 }
+
+resource "aws_lb_target_group" "tg_home" {
+  name     = "tg-home"
+  port     = 80
+  protocol = "HTTP"
+  health_check {
+    path = "/"
+    port = 80
+    protocol = "TCP"
+  }
+}
+
+resource "aws_lb_target_group" "tg_home" {
+  name     = "tg-home"
+  port     = 80
+  protocol = "HTTP"
+  health_check {
+    path = "/"
+    port = 80
+    protocol = "TCP"
+  }
+}
