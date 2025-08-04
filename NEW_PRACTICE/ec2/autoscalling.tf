860379e101a3006adb652ac9ec26b38089169428
AkshayParsade <adparsade@gmail.com>
2025-08-04 16:13:23 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/autoscalling.tf
+++ NEW_PRACTICE/ec2/autoscalling.tf
@@ -65,7 +65,7 @@ resource "aws_autoscaling_group" "asg_groccary" {
   max_size            = 5
   min_size            = 2
   launch_template {
-    id =
+    id = aws
     version = 
   }
   vpc_zone_identifier = [var.subnet_id_1, var.subnet_id_2]
