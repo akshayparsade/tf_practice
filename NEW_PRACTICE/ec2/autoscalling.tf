eba9b9790fc846f543e1944c18f6226bf0a66391
AkshayParsade <adparsade@gmail.com>
2025-08-04 16:16:34 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/autoscalling.tf
+++ NEW_PRACTICE/ec2/autoscalling.tf
@@ -93,7 +93,7 @@ resource "aws_autoscaling_group" "asg_home" {
   min_size            = 2
   launch_template {
     id = aws_launch_template.lc_home.id
-    version = aws_launch_template.lc_home
+    version = aws_launch_template.lc_home.latest_version
   }
   vpc_zone_identifier = [var.subnet_id_1, var.subnet_id_2]
   target_group_arns = [ aws_lb_target_group.tg_home ]
