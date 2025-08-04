9362f2992a199bad62e5cd26e0fd32f042a1b94c
AkshayParsade <adparsade@gmail.com>
2025-08-04 14:06:14 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/Lbr.tf
+++ NEW_PRACTICE/ec2/Lbr.tf
@@ -44,7 +44,7 @@ resource "aws_lb" "my_lb" {
   }
 }
 
-resource "aws_lb_listener" "my_lb_listener_rule_groccary" {
+resource "aws_lb_listener" "my_lb_listener__groccary" {
   load_balancer_arn = aws_lb.my_lb.arn
   port              = "80"
   protocol          = "HTTP"
