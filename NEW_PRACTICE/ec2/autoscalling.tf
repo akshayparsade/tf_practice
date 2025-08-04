fae5f359fc9459eec4018321e4fb1ac14345d54c
AkshayParsade <adparsade@gmail.com>
2025-08-04 16:09:52 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/autoscalling.tf
+++ NEW_PRACTICE/ec2/autoscalling.tf
@@ -33,7 +33,7 @@ resource "aws_launch_template" "lc_groccary" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name =var.key_name
-    vpc_security_groups_ids = aws_security_group.lc_sg.id }
+    vpc_security_groups_ids = [ aws_security_group.lc_sg.id ]
     user_data = <<EOF
     #!/bin/bash
     apt install httpd -y
