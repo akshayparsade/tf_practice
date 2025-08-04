eeb5f67cf89b9eb31315973ad09eddc3b9354e9f
AkshayParsade <adparsade@gmail.com>
2025-08-04 13:43:58 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/Lbr.tf
+++ NEW_PRACTICE/ec2/Lbr.tf
@@ -25,7 +25,7 @@ resource "aws_lb_target_group" "tg_elctronix" {
   port     = 80
   protocol = "HTTP"
   health_check {
-    path = "/electro"
+    path = "/elctronix"
     port = 80
     protocol = "TCP"
   }
