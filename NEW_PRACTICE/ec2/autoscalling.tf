6828936afde20919d7dc76e791b7807d4593a2ad
AkshayParsade <adparsade@gmail.com>
2025-08-04 10:52:03 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/ec2/autoscalling.tf
+++ NEW_PRACTICE/ec2/autoscalling.tf
@@ -29,7 +29,7 @@ resource "aws_launch_configuration" "lc_home" {
     EOF 
 }
 
-resource "aws_launch_configuration" "lc_home" {
+resource "aws_launch_configuration" "lc_el" {
     image_id = var.ami_id
     instance_type = var.instance_type
     key_name =var.key_name
