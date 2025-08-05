f8d8231fbfd71ea932f47a543c66a450bc6735d7
AkshayParsade <adparsade@gmail.com>
2025-08-05 15:56:08 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/main.tf
+++ NEW_PRACTICE/akkitech/main.tf
@@ -12,12 +12,12 @@ private_subnet_cidr = var.private_subnet_cidr
 public_subnet_cidr = var.public_subnet_cidr
 }
 
-module "akkitech_ec2" {
-  source = "./modules/ec2"
-  ami = var.image_id
-  instance_type = var.instance_type
-  key_pair = var.key_pair
-  security_groups_id = var.security_groups_id
-  private_subnet_id = var.pri_sub_id
-  public_subnet_id = var.p
-}
+# module "akkitech_ec2" {
+#   source = "./modules/ec2"
+#   ami = var.image_id
+#   instance_type = var.instance_type
+#   key_pair = var.key_pair
+#   security_groups_id = var.security_groups_id
+#   private_subnet_id = var.pri_sub_id
+#   public_subnet_id = var.pub_sub_id
+# }
