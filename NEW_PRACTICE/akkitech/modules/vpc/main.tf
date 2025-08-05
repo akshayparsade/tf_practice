65f7133acff10d23863d9602052a1b1f16c4f25e
AkshayParsade <adparsade@gmail.com>
2025-08-05 14:49:40 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/modules/vpc/main.tf
+++ NEW_PRACTICE/akkitech/modules/vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "akkitec_public_subnet" {
   }
 }
 
-resource "aws_subnet" "akkitec_public_subnet" {
+resource "aws_subnet" "akkitec_private_subnet" {
   vpc_id            = aws_vpc.akkitech_vpc.id
   cidr_block        = var.public_subnet_cidr
   #availability_zone = "us-east-1a"  # Change if needed
