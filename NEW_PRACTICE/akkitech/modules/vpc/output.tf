490402a1e90ac99c471da5f272cc77649bd33f8c
AkshayParsade <adparsade@gmail.com>
2025-08-05 16:40:29 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/modules/vpc/output.tf
+++ NEW_PRACTICE/akkitech/modules/vpc/output.tf
@@ -1,7 +1,8 @@
-# output "private_subnet_id" {
-#   value = module.akkitech_vpc.private_subnet_id
-# }
+output "private_subnet_id" {
+  value = module.akkitech_vpc.private_subnet_id
+}
 
-# output "public_subnet_id" {
-#   value = module.akkitech_vpc.public_subnet_id
-# }
+output "public_subnet_id" {
+  value = module.akkitech_vpc.public_subnet_id
+}
+
