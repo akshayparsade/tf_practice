dcea68f784fed2772fec8f51de614bcc245fa19c
AkshayParsade <adparsade@gmail.com>
2025-08-05 14:47:10 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/modules/vpc/main.tf
+++ NEW_PRACTICE/akkitech/modules/vpc/main.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "akkitech_vpc" {
   }
 }
 
+resource "aws_subnet" "akkipublic_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"  # Change if needed
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
