de6eb455bf0b74202b34824cb5689b0dbe403c89
AkshayParsade <adparsade@gmail.com>
2025-08-05 15:22:59 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/modules/ec2/main.tf
+++ NEW_PRACTICE/akkitech/modules/ec2/main.tf
@@ -1,7 +1,15 @@
 resource "aws_instance" "private_instance" {
   ami = var.image_id
   instance_type = var.instance_type
-  key_name = var.key_pa
+  key_name = var.key_pair
+  vpc_security_group_ids = var.security_groups_id
+  subnet_id = var.private_subnet.id
+}
+
+resource "aws_instance" "private_instance" {
+  ami = var.image_id
+  instance_type = var.instance_type
+  key_name = var.key_pair
   vpc_security_group_ids = var.security_groups_id
   subnet_id = var.private_subnet.id
 }
