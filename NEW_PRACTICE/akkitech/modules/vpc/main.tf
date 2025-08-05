92b7c88a3b52c74254064ea2ba05a665dce5e3e8
AkshayParsade <adparsade@gmail.com>
2025-08-05 15:01:23 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/modules/vpc/main.tf
+++ NEW_PRACTICE/akkitech/modules/vpc/main.tf
@@ -41,7 +41,7 @@ resource "aws_route_table" "akkitech_public_rt" {
   vpc_id = aws_vpc.akkitech_vpc.id
 
   route {
-    cidr_block = var
+    cidr_block = var.private_subnet_cidr
     gateway_id = aws_internet_gateway.igw.id
   }
 
