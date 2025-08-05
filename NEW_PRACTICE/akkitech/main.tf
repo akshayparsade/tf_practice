b4b7cb51347c71381ad36a43b6598aa0be7e5926
AkshayParsade <adparsade@gmail.com>
2025-08-05 16:42:19 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/main.tf
+++ NEW_PRACTICE/akkitech/main.tf
@@ -18,7 +18,7 @@ module "akkitech_ec2" {
   instance_type = var.instance_type
   key_pair = var.key_pair
   security_groups_id = aws_security_group.ec2_sg
-  private_subnet_id = module.akkitech_vpc
+  private_subnet_id = module.akkitech_vpc.private_subnet_id
   public_subnet_id = var.pub_sub_id
 }
 
