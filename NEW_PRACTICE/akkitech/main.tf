b857802b69ae4381dd681a04dc221962b954172d
AkshayParsade <adparsade@gmail.com>
2025-08-05 15:46:36 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/main.tf
+++ NEW_PRACTICE/akkitech/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 module "akkitech_vpc" {
  source = "./modules/vpc"
 cidr = var.vpc_cidr
-project = "akkitrch.sol"
+project = var.project
 env = "dev"
 private_subnet_cidr = "10.0.0.0/20"
 public_subnet_cidr = "10.0.0.0/20"
