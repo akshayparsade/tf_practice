fdd4862911b3d97271d354cac4e1ff39f8d3d900
AkshayParsade <adparsade@gmail.com>
2025-08-05 15:42:35 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/modules/ec2/variable.tf
+++ NEW_PRACTICE/akkitech/modules/ec2/variable.tf
@@ -2,11 +2,8 @@ variable "image_id" {}
 variable "instance_type" {}
 variable "key_pair" {}
 
-variable "security_groups_id" {
+variable "security_groups_id" {}
 
-variable "private_subnet_id" {
-    default = 
-}
+variable "private_subnet_id" {}
+
 variable "public_subnet_id" {
-    default = 
-}
