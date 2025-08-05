5e8d680484b347a845f461a2f3d037e7ede8f264
AkshayParsade <adparsade@gmail.com>
2025-08-05 15:07:05 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/modules/vpc/main.tf
+++ NEW_PRACTICE/akkitech/modules/vpc/main.tf
@@ -40,7 +40,7 @@ resource "aws_internet_gateway" "akkitech_igw" {
 resource "aws_default_route_table" "defa_rt" {
   default_route_table_id = aws_vpc.akkitech_vpc.default_route_table_id
   route {
-    cidr_block = "0.0.0.0/0
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
