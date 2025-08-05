a9ba97999fa7333c587368ab37ee8bca83694893
AkshayParsade <adparsade@gmail.com>
2025-08-05 15:50:27 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/main.tf
+++ NEW_PRACTICE/akkitech/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "akkitech_vpc" {
- source = "./modules/vpc"
+source = "./modules/vpc"
 cidr = var.vpc_cidr
 project = var.project
 env = var.env
@@ -14,5 +14,5 @@ public_subnet_cidr = var.public_subnet_cidr
 
 module "akkitech_ec2" {
   source = "./modules/ec2"
-  
+
 }
