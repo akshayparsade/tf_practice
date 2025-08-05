075300ff094d8499d5f1798724bb707d571711cc
AkshayParsade <adparsade@gmail.com>
2025-08-05 16:21:34 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/modules/vpc/output.tf
+++ NEW_PRACTICE/akkitech/modules/vpc/output.tf
@@ -1,8 +1,8 @@
 output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
+  value = aws_subnet.private_subnet_cidr.id
 }
 
 output "public_subnet_id" {
-   value = aws_subnet.public_subnet.id
+   value = aws_subnet.public_subnetcidr.id.id
 
 }
