719935e5f09f62e0a02c0e308e7e360465ef8a28
AkshayParsade <adparsade@gmail.com>
2025-08-05 15:46:46 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/akkitech/main.tf
+++ NEW_PRACTICE/akkitech/main.tf
@@ -7,7 +7,7 @@ module "akkitech_vpc" {
  source = "./modules/vpc"
 cidr = var.vpc_cidr
 project = var.project
-env = "dev"
-private_subnet_cidr = "10.0.0.0/20"
+env = var.env
+private_subnet_cidr = var.
 public_subnet_cidr = "10.0.0.0/20"
 }
