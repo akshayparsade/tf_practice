ad0e3d5d0653372cc4cbc9a3231d63aaef5355aa
AkshayParsade <adparsade@gmail.com>
2025-08-07 15:06:28 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/provisnor/main.tf
+++ NEW_PRACTICE/provisnor/main.tf
@@ -1,10 +1,9 @@
 provider "aws" {
-    region = ""
+    region = "us-east-1"
   
 }
 
 resource "aws_instance" "count_ec2" {
- #   count = 3
     ami = "ami-08a6efd148b1f7504"
     instance_type = "t2.micro"
     key_name = "New-Virgi-key"
