925d30f86bdc47491824c3b1d7c353216605ac92
AkshayParsade <adparsade@gmail.com>
2025-08-07 09:59:35 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/loop/main.tf
+++ NEW_PRACTICE/loop/main.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "loop_ec2" {
+    count
     ami = "ami-08a6efd148b1f7504"
     instance_type = "t2.micro"
     key_name = "New-Virgi-key"
     tags = {
       Name = "instance-${value}"
-      Env = terr
+      Env = terraform.workspace
     }
   
 }
