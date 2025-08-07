b0caf05bb22455414c59757dd46ae592cb29c282
AkshayParsade <adparsade@gmail.com>
2025-08-07 10:01:05 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/loop/main.tf
+++ NEW_PRACTICE/loop/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   
 }
 
-# resource "aws_instance" "loop_ec2" {
+# resource "aws_instance" "count_ec2" {
 #     count = 3
 #     ami = "ami-08a6efd148b1f7504"
 #     instance_type = "t2.micro"
@@ -15,6 +15,6 @@ provider "aws" {
   
 # }
 
-resource "aws_instance" "name" {
+resource "aws_instance" "for" {
   
 }
