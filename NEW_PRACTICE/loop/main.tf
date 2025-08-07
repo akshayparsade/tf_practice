3b9ccf6dfbd6c648bf162635d9198f4ad439c137
AkshayParsade <adparsade@gmail.com>
2025-08-07 13:44:35 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/loop/main.tf
+++ NEW_PRACTICE/loop/main.tf
@@ -20,30 +20,30 @@ provider "aws" {
   
 # }
 
-resource "aws_instance" "for_each_ec2" {
-    for_each = var.instance_type
-    ami = "ami-08a6efd148b1f7504"
-    instance_type = each.value
-    key_name = "New_Virgi_key"
-    tags = {
-        Name = "instance-${each.key}"
-        Env = terraform.workspace
-    }   
-}
+# resource "aws_instance" "for_each_ec2" {
+#     for_each = var.instance_type
+#     ami = "ami-08a6efd148b1f7504"
+#     instance_type = each.value
+#     key_name = "New_Virgi_key"
+#     tags = {
+#         Name = "instance-${each.key}"
+#         Env = terraform.workspace
+#     }   
+# }
 
-variable "instance_type" {
-   default = {
-       micro = "t2.micro"
-     #  small = "t2.small"
-     #  medium = "t2.medium"
+# variable "instance_type" {
+#    default = {
+#        micro = "t2.micro"
+#      #  small = "t2.small"
+#      #  medium = "t2.medium"
   
-   }
-}
+#    }
+# }
 
-output "instance_type" {
-    value = [for type in var.instance_type : "${type}"]
+# output "instance_type" {
+#     value = [for type in var.instance_type : "${type}"]
 
-}
+# }
 
 # output "public_ip" {
 #     value = [for ip in aws_instance.for_each_ec2.public_ip : "${ip}"]
