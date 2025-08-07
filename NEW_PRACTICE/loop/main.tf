e8dae7f5254fba98cc7e6c4f8e22ea1afce518da
AkshayParsade <adparsade@gmail.com>
2025-08-07 09:58:45 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/loop/main.tf
+++ NEW_PRACTICE/loop/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "loop_ec2" {
     ami = "ami-08a6efd148b1f7504"
     instance_type = "t2.micro"
-    key_name = "New-Virgi-"
+    key_name = "New-Virgi-key"
+    tags = 
   
 }
