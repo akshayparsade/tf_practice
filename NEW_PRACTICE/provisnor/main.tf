d0a252d43b06dc1ff459bb8b86ec3d145b1e4837
AkshayParsade <adparsade@gmail.com>
2025-08-07 16:57:08 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/provisnor/main.tf
+++ NEW_PRACTICE/provisnor/main.tf
@@ -7,9 +7,9 @@ resource "aws_instance" "count_ec2" {
     ami = "ami-08a6efd148b1f7504"
     instance_type = "t2.micro"
     key_name = "New_Virgi_key"
-    tags = {
-      Name = "provisnor"
-    }
+    # tags = {
+    #   Name = "provisnor"
+    # }
 
     provisioner "file" {
        source = "./script.sh"
