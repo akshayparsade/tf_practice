7b23173fadaad1414008f9931d4051d739a97617
AkshayParsade <adparsade@gmail.com>
2025-08-08 12:03:47 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/eks/main.tf
+++ NEW_PRACTICE/eks/main.tf
@@ -1,4 +1,20 @@
 resource "aws_iam_role "eks_cluster_role" {
     name = "eks-cluster-role"
+
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "eks.amazonaws.com"
+                ]
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+
 }
 
