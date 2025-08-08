d137dee1863bb1a5ef26b2f96f767b4f15ba855e
AkshayParsade <adparsade@gmail.com>
2025-08-08 14:43:28 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/eks/main.tf
+++ NEW_PRACTICE/eks/main.tf
@@ -60,3 +60,58 @@ resource "aws_iam_role_policy_attachment" "node_ECR_ReadOnly" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+##########################
+# EKS Cluster
+##########################
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = concat(aws_subnet.private[*].id)
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+    endpoint_public_access = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSServicePolicy
+  ]
+  tags = { Name = var.cluster_name }
+}
+
+##########################
+# EKS Managed Node Group
+##########################
+resource "aws_eks_node_group" "managed_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${var.cluster_name}-nodegroup"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+
+  subnet_ids = aws_subnet.private[*].id
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  instance_types = var.node_instance_types
+
+  remote_access {
+    # Key name must exist in your account if you enable this. Optional.
+    # ec2_ssh_key = "my-key"
+    # source_security_group_ids = [aws_security_group.eks_node_sg.id]
+  }
+
+  tags = {
+    Name = "eks-managed-nodes"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node_ECR_ReadOnly
+  ]
+}
