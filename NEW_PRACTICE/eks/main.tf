4bfaa9ef0ea618ba0ccd879efdea7fbc6944f9a1
AkshayParsade <adparsade@gmail.com>
2025-08-08 14:34:56 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/eks/main.tf
+++ NEW_PRACTICE/eks/main.tf
@@ -5,7 +5,7 @@
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role-${var.cluster_name}"
   assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role.json
-}
+
 
 data "aws_iam_policy_document" "eks_cluster_assume_role" {
   statement {
