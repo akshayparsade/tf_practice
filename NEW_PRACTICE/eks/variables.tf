0b1c13b77961a82a47c31935abed5da84ec2f919
AkshayParsade <adparsade@gmail.com>
2025-08-08 14:47:39 +0530

    --- Auto Git Commit ---

---
+++ NEW_PRACTICE/eks/variables.tf
@@ -0,0 +1,47 @@
+##########################
+# Variables
+##########################
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "demo-eks-cluster"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "node_instance_types" {
+  type    = list(string)
+  default = ["t3.medium"]
+}
+
+variable "desired_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "min_size" {
+  type    = number
+  default = 1
+}
+
+variable "max_size" {
+  type    = number
+  default = 3
+}
