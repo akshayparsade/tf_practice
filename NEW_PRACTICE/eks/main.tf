68c4afeafadc5af4d31d58ccd31ae4414e29a155
AkshayParsade <adparsade@gmail.com>
2025-08-08 14:36:16 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/eks/main.tf
+++ NEW_PRACTICE/eks/main.tf
@@ -18,6 +18,7 @@ data "aws_iam_policy_document" "eks_cluster_assume_role" {
   }
 }
 
+# Attache polices to EKS
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
