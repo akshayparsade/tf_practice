ec3206af295cd16864e110868f06863fca1f9b34
AkshayParsade <adparsade@gmail.com>
2025-08-08 14:50:20 +0530

    --- Auto Git Commit ---

--- NEW_PRACTICE/eks/main.tf
+++ NEW_PRACTICE/eks/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 ##########################
 # IAM Roles & Policies for EKS
 ##########################
